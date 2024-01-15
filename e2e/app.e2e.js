f340c5c8b9e6fb17002ebda26f268e00dd020bfe
unknown <lorenzopenalver3@gmail.com>
2024-01-15 16:21:29 -0300

    Buenas prácticas en pruebas E2E

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,22 +1,32 @@
 const request = require('supertest')
 const express = require('express')
 
-const app = express();
 
-app.get('/hello', (req, res) => {
-  res.status(200).json({name: "nico"})
-});
-
-app.listen(8000)
-
-const api = request(app)
 
 describe('test for app', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = express();
+
+    app.get('/hello', (req, res) => {
+      res.status(200).json({name: "nico"})
+    });
+    server = app.listen(8000)
+    api = request(app)
+  });
+
   test('GET /hello', async () => {
   const response = await api.get('/hello');
   expect(response).toBeTruthy();
   expect(response.statusCode).toEqual(200);
   expect(response.body.name).toEqual("nico");
   expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  afterEach(() => {
+    server.close()
   });
 });
