2617531dce1379de97d6901fd20c7cf5b8bda3b4
unknown <lorenzopenalver3@gmail.com>
2024-01-17 16:48:05 -0300

    Mocking en Node.js

--- e2e/auth.e2e.js
+++ e2e/auth.e2e.js
@@ -4,7 +4,14 @@ const createApp = require('../src/app.js');
 const { models } = require('../src/db/sequelize.js');
 const { upSeed, downSeed } = require('./utils/umzug.js');
 
-
+const mockSendMail = jest.fn();
+jest.mock('nodemailer', () => {
+  return {createTransport: jest.fn().mockImplementation(() => {
+    return {
+      sendMail: mockSendMail
+    }
+  })}
+})
 
 describe('test for /auth path', () => {
   let app = null;
@@ -27,7 +34,6 @@ describe('test for /auth path', () => {
       const { statusCode} = await api.post('/api/v1/auth/login').send(inputData)
       expect(statusCode).toBe(401);
     });
-
     test('should return a 200', async () => {
       const user = await models.User.findByPk('1')
       const inputData = {
@@ -40,13 +46,36 @@ describe('test for /auth path', () => {
       expect(body.user.email).toEqual(user.email);
       expect(body.user.password).toBeUndefined();
     });
-
-
-
   afterAll(async () => {
     await downSeed
     server.close()
   });
 });
+
+describe('POST /recovery', () => {
+  beforeAll(()=>{
+    mockSendMail.mockClear();
+  })
+  test('should return a 401', async () => {
+    const inputData = {
+      email: 'emaifake@mail.com',
+    }
+    const { statusCode} = await api.post('/api/v1/auth/recovery').send(inputData)
+    expect(statusCode).toBe(401);
+  });
+
+  test('should send mail', async () => {
+    const user = await models.User.findByPk('1')
+    const inputData = {
+      email: user.email,
+    }
+    mockSendMail.mockResolvedValue(true)
+    const { statusCode, body} = await api.post('/api/v1/auth/recovery').send(inputData)
+    expect(statusCode).toBe(200);
+    expect(body.message).toEqual('mail sent');
+    expect(mockSendMail).toHaveBeenCalled();
+  });
+});
+
 
 })
