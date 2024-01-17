2ff95a4a59388bed9b68c5f65f21e73c41b31c69
unknown <lorenzopenalver3@gmail.com>
2024-01-17 11:16:05 -0300

    Pruebas a rutas con protección

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 // const express = require('express')
-const createApp = require('../src/app.js')
+const createApp = require('../src/app.js');
+const { config } = require('../src/config/config.js');
 
 
 describe('test for app', () => {
@@ -21,6 +22,26 @@ describe('test for app', () => {
   expect(response.body.name).toEqual("nico");
   expect(response.headers['content-type']).toMatch(/json/);
   });
+
+  describe('GET /nueva-rut', () => {
+    test('should return 401', async () => {
+      const {statusCode } = await api.get('/nueva-ruta');
+      expect(statusCode).toEqual(401);
+
+      });
+
+    test('should return 200', async () => {
+      const {statusCode } = await api.get('/nueva-ruta').set({ api: config.apiKey});
+      expect(statusCode).toEqual(200);
+
+      });
+    test('should return 401 invalid APIKey', async () => {
+      const {statusCode } = await api.get('/nueva-ruta').set({ api: '2000'});
+      expect(statusCode).toEqual(401);
+
+      });
+  });
+
 
   afterAll(() => {
     server.close()
